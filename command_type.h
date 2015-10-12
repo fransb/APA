9955b7a9e180ae6090f9932d1a8adc6be61989c1
Frans Bergquist <frans.b@gmail.com>
2015-10-12 21:20:48 +0200

    everything is broken

--- command_type.h
+++ command_type.h
@@ -1,5 +1,5 @@
-#ifndef CONFIG_H
-#define CONGIG_H
+#ifndef COMMAND_TYPE_H
+#define COMMAND_TYPE_H
 
 #include "Cosa/Types.h"
 
@@ -7,12 +7,20 @@ namespace APA {
 
   enum command_t {
     AWAKE,
-    SEND_ANALOG
+    SEND_ANALOG_PIN,
+    ANALOG_PIN_VALUE,
+    SLEEP
   } __attribute__((packed));
 
   struct message_t {
     command_t command;
     uint16_t payload;
-  };
+  } __attribute__((packed));
+  
+  static const uint32_t wait = 400;
+  
+  static const uint8_t CLIENT_ID = 0x80;
+  static const uint8_t SERVER_ID = 0x81;
+  static const uint8_t PING_TYPE = 0x80;
 }
 #endif
