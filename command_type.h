25154516d3f0b7d47867ad501441241a5cb1805c
Frans Bergquist <frans.b@gmail.com>
2015-10-12 22:34:06 +0200

    removed namespace

--- command_type.h
+++ command_type.h
@@ -4,23 +4,25 @@
 #include "Cosa/Types.h"
 
 namespace APA {
-
-  enum command_t {
-    AWAKE,
-    SEND_ANALOG_PIN,
-    ANALOG_PIN_VALUE,
-    SLEEP
-  } __attribute__((packed));
-
-  struct message_t {
-    command_t command;
-    uint16_t payload;
-  } __attribute__((packed));
+  // static const CLIENT_ID = 0x80;
+  // static const SERVER_ID = 0x81;
+ static const uint32_t wait = 400;
   
-  static const uint32_t wait = 400;
-  
-  static const uint8_t CLIENT_ID = 0x80;
-  static const uint8_t SERVER_ID = 0x81;
-  static const uint8_t PING_TYPE = 0x80;
+ static const uint8_t PING_TYPE = 0x80;
+
 }
+
+enum command_t {
+  AWAKE,
+  SEND_ANALOG_PIN,
+  ANALOG_PIN_VALUE,
+  SLEEP
+} __attribute__((packed));
+
+struct message_t {
+  command_t command;
+  uint16_t payload;
+} __attribute__((packed));
+
+//}
 #endif
